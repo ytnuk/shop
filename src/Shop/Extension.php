0d4c6dcb2acd866c4cda77c5ebda6cdb54312733
Vít Kutný <vitkutny@outlook.com>
2014-05-27 02:50:25 +0200

    dev

--- src/Shop/Extension.php
+++ src/Shop/Extension.php
@@ -5,5 +5,15 @@ namespace WebEdit\Shop;
 use WebEdit;
 
 final class Extension extends WebEdit\Extension {
-    
+
+    public function loadConfiguration() { //TODO
+        $builder = $this->getContainerBuilder();
+        $extension = new WebEdit\Reflection($this);
+        $file = dirname(dirname($extension->getFileName())) . '/config.neon';
+        if (file_exists($file)) {
+            $config = $this->loadFromFile($file);
+            $this->compiler->parseServices($builder, $config);
+        }
+    }
+
 }
