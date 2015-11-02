fdab5f9dcad99359c41fc4d7da64713e69c964df
Vít Kutný <vitkutny@icloud.com>
2015-11-03 00:05:41 +0100

    Extension refactor

--- src/Shop/Extension.php
+++ src/Shop/Extension.php
@@ -7,33 +7,37 @@ use Ytnuk;
 
 final class Extension
 	extends Nette\DI\CompilerExtension
-	implements Ytnuk\Config\Provider
+	implements Ytnuk\Orm\Provider, Kdyby\Translation\DI\ITranslationProvider
 {
 
-	public function getConfigResources() : array
+	public function loadConfiguration()
+	{
+		parent::loadConfiguration();
+		$builder = $this->getContainerBuilder();
+		$builder->addDefinition($this->prefix('control'))->setImplement(Control\Factory::class);
+		$builder->addDefinition($this->prefix('category.control'))->setImplement(Category\Control\Factory::class);
+		$builder->addDefinition($this->prefix('category.form.control'))->setImplement(Category\Form\Control\Factory::class);
+		$builder->addDefinition($this->prefix('product.control'))->setImplement(Product\Control\Factory::class);
+		$builder->addDefinition($this->prefix('product.form.control'))->setImplement(Product\Form\Control\Factory::class);
+	}
+
+	public function getTranslationResources() : array
 	{
 		return [
-			Ytnuk\Orm\Extension::class => [
-				'repositories' => [
-					$this->prefix('categoryRepository') => Category\Repository::class,
-					$this->prefix('categoryDescriptionRepository') => Category\Description\Repository::class,
-					$this->prefix('productRepository') => Product\Repository::class,
-					$this->prefix('productDescriptionRepository') => Product\Description\Repository::class,
-					$this->prefix('productContentRepository') => Product\Content\Repository::class,
-					$this->prefix('productCategoryRepository') => Product\Category\Repository::class,
-				],
-			],
-			Kdyby\Translation\DI\TranslationExtension::class => [
-				'dirs' => [
-					__DIR__ . '/../../locale',
-				],
-			],
-			'services' => [
-				Control\Factory::class,
-				Category\Control\Factory::class,
-				Category\Form\Control\Factory::class,
-				Product\Control\Factory::class,
-				Product\Form\Control\Factory::class,
+			__DIR__ . '/../../locale',
+		];
+	}
+
+	public function getOrmResources() : array
+	{
+		return [
+			'repositories' => [
+				$this->prefix('categoryRepository') => Category\Repository::class,
+				$this->prefix('categoryDescriptionRepository') => Category\Description\Repository::class,
+				$this->prefix('productRepository') => Product\Repository::class,
+				$this->prefix('productDescriptionRepository') => Product\Description\Repository::class,
+				$this->prefix('productContentRepository') => Product\Content\Repository::class,
+				$this->prefix('productCategoryRepository') => Product\Category\Repository::class,
 			],
 		];
 	}
