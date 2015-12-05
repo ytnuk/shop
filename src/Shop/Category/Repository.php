03bcff251ea611c2645559301ca026db3671e16f
Vít Kutný <vitkutny@icloud.com>
2015-12-05 01:03:18 +0100

    implement Route\Filter\Out for module entities

--- src/Shop/Category/Repository.php
+++ src/Shop/Category/Repository.php
@@ -1,10 +1,12 @@
 <?php
 namespace Ytnuk\Shop\Category;
 
+use Nette;
 use Ytnuk;
 
 final class Repository
 	extends Ytnuk\Orm\Repository
+	implements Ytnuk\Web\Domain\Router\Filter\Out
 {
 
 	public static function getEntityClassNames() : array
@@ -12,5 +14,27 @@ final class Repository
 		return [
 			Entity::class,
 		];
+	}
+
+	public function filterOut(
+		array $params,
+		array & $dependencies = []
+	) : array
+	{
+		if ($params['presenter'] === 'Shop:Category:Presenter' && $id = $params['id'] ?? NULL) {
+			if ($entity = $this->getById($id)) {
+				if ($entity instanceof Ytnuk\Cache\Provider) {
+					$dependencies = array_merge_recursive(
+						$dependencies,
+						[
+							Nette\Caching\Cache::TAGS => $entity->getCacheTags(),
+						]
+					);
+				}
+				$params['slug'] = Nette\Utils\Strings::webalize($entity);
+			}
+		}
+
+		return $params;
 	}
 }
--- src/Shop/Product/Repository.php
+++ src/Shop/Product/Repository.php
@@ -1,11 +1,13 @@
 <?php
 namespace Ytnuk\Shop\Product;
 
+use Nette;
 use Nextras;
 use Ytnuk;
 
 final class Repository
 	extends Ytnuk\Orm\Repository
+	implements Ytnuk\Web\Domain\Router\Filter\Out
 {
 
 	public function findAll() : Nextras\Orm\Collection\ICollection
@@ -21,5 +23,27 @@ final class Repository
 		return [
 			Entity::class,
 		];
+	}
+
+	public function filterOut(
+		array $params,
+		array & $dependencies = []
+	) : array
+	{
+		if ($params['presenter'] === 'Shop:Product:Presenter' && $id = $params['id'] ?? NULL) {
+			if ($entity = $this->getById($id)) {
+				if ($entity instanceof Ytnuk\Cache\Provider) {
+					$dependencies = array_merge_recursive(
+						$dependencies,
+						[
+							Nette\Caching\Cache::TAGS => $entity->getCacheTags(),
+						]
+					);
+				}
+				$params['slug'] = Nette\Utils\Strings::webalize($entity);
+			}
+		}
+
+		return $params;
 	}
 }
