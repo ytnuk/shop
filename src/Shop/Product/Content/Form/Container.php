6f7714405386913d3c8824b382e97e29d635c145
Vít Kutný <vitkutny@icloud.com>
2015-12-31 15:08:23 +0100

    translation/form/container

--- src/Shop/Product/Content/Form/Container.php
+++ src/Shop/Product/Content/Form/Container.php
@@ -7,20 +7,4 @@ final class Container
 	extends Ytnuk\Orm\Form\Container
 {
 
-	public function setValues(
-		$values,
-		$erase = FALSE
-	) : Ytnuk\Orm\Form\Container
-	{
-		if (isset($values['value'], $values['value']['translates']) && (array) $values['value']['translates']) {
-			return parent::setValues(
-				(array) $values,
-				$erase
-			);
-		} else {
-			$this->removeEntity(FALSE);
-
-			return $this;
-		}
-	}
 }
